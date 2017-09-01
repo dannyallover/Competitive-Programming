349c675ebb749a2873a4252a54bdbdb20ee8771f
Danny Halawi <dannyhalawi15@gmail.com>
2017-08-31 18:08:49 -0700

    Update LuckyYear.cpp

--- Lucky Year/LuckyYear.cpp
+++ Lucky Year/LuckyYear.cpp
@@ -6,28 +6,9 @@ using namespace std;
 int main(void) {
 	string num;
 	cin >> num;
-	bool x = true;
 
-	int totalYears = 0;
-	if(num.length() == 1) {
-		cout << 1;
-		exit(0);
-	}
-1004
-	for(int i = 1; i < num.length(); i++) {
-		totalYears += (10 - stoi(num.substr(i, 1))) * pow(10, num.length() - (i+1));
-			if(i + 1 != num.length())
-				if(stoi(num.substr(i+1, 1)) != 0)
-					totalYears -= pow(10, num.length() - (i+1));
-		if(stoi(num.substr(i,1)) != 0) {
-			x = false;
-		}
-	}
+	int num1 = stoi(num);
+	int num2 = (stoi(num.substr(0,1)) + 1) * pow(10, num.length() - 1);
 
-	if(x)
-		cout << (long)pow(10, num.length() - 1);
-	else
-		cout << totalYears;
-
-	return 0;
+	cout << num2 - num1;
 }
